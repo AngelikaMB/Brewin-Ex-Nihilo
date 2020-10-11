7b76d633a77107565cc5f3d3537f2ef671fc904c
Sonyei <esonyei@gmail.com>
2020-10-11 19:53:42 -0400

    added Styled Components

--- src/index.js
+++ src/index.js
@@ -3,13 +3,14 @@ import ReactDOM from 'react-dom'
 // import {BrowserRouter as Router} from 'react-router-dom'
 import './App.css'
 import App from './App'
+import { ThemeProvider } from 'styled-components'
+import Theme from '../src/Theme/Theme'
 
 ReactDOM.render (
 
-/* <Router> */
+<ThemeProvider theme={Theme}>
 <App /> 
-/* </Router> */
-
+</ThemeProvider>
 
 , document.getElementById('root')
 );
